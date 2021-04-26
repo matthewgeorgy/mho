d6f4746eea37f1b01c6486e365337aa73323c1d2
Matthew <matthewgeorgy61@gmail.com>
2021-04-26 17:30:47 -0400

    added documentation (comments); retab

--- mho_array.h
+++ mho_array.h
@@ -1,62 +1,90 @@
 #ifndef MHO_ARRAY_H
 #define MHO_ARRAY_H
 
+/*
+    Inspired GREATLY by John Jackson's "gs_dyn_array" implementation. Almost
+    identical but has a few differences.
+*/
+
 #include <stdlib.h>
 #include <types.h>
 
 typedef struct _TAG_m_array_header
 {
-	u32 size;       // number of elements in array containing data
-	u32 capacity;   // total number of elements
+    u32 size;       // number of elements in array containing data
+    u32 capacity;   // total number of elements
 } m_array_header_t;
 
+// Wrapper macro
+#define m_array(__type) \
+    __type *
+
+// Initializes the array
 void **m_array_init(void *arr, usize val_size);
+
+// Resizes the array
 void *m_array_resize(void *arr, usize sz, usize amt);
 
+// Returns the header of the array
 #define m_array_head(__array) \
-	((m_array_header_t *)(__array) - 1)
+    ((m_array_header_t *)(__array) - 1)
 
+// Returns the size of the array
 #define m_array_size(__array) \
-	(__array == NULL ? 0 : m_array_head(__array)->size)
+    (__array == NULL ? 0 : m_array_head(__array)->size)
 
+// Returns the capacity of the array
 #define m_array_capacity(__array) \
-	(__array == NULL ? 0 : m_array_head(__array)->capacity)
+    (__array == NULL ? 0 : m_array_head(__array)->capacity)
 
+// Returns whether the array is full
 #define m_array_full(__array) \
-	((m_array_size(__array)) == (m_array_capacity(__array)))
+    ((m_array_size(__array)) == (m_array_capacity(__array)))
 
+// Returns whether the array is empty
 #define m_array_empty(__array) \
-	(m_array_init((void **)&(__array), sizeof(*(__array)), (m_array_size(__array) == 0)))
+    (m_array_init((void **)&(__array), sizeof(*(__array)), (m_array_size(__array) == 0)))
 
+// Frees the array
 #define m_array_free(__array) \
-	(free(m_array_head(__array))) // tighten with NULL checks
+    (free(m_array_head(__array))) // tighten with NULL checks
 
+// Returns whether the array needs to grow by a given size
 #define m_array_need_grow(__array, __n) \
-	((__array) == 0 || m_array_size(__array) + (__n) > m_array_capacity(__array))
+    ((__array) == 0 || m_array_size(__array) + (__n) > m_array_capacity(__array))
 
+// Grows the array (doubles in size)
 #define m_array_grow(__array) \
-	m_array_resize((__array), sizeof(*(__array)), m_array_capacity(__array) ? m_array_capacity(__array) * 2 : 1)
+    m_array_resize((__array), sizeof(*(__array)), m_array_capacity(__array) ? m_array_capacity(__array) * 2 : 1)
 
+// Pushes data to the back of the array
 #define m_array_push(__array, __data) \
-	do \
-	{ \
-		m_array_init((void **)&(__array), sizeof(*(__array))); \
-		if (!(__array) || ((__array) && m_array_need_grow(__array, 1))) \
-		{ \
-			*((void **)&(__array)) = m_array_grow(__array); \
-		} \
-		(__array)[m_array_size(__array)] = (__data); \
-		m_array_head(__array)->size++; \
-	} while (0)
+    do \
+    { \
+        m_array_init((void **)&(__array), sizeof(*(__array))); \
+        if (!(__array) || ((__array) && m_array_need_grow(__array, 1))) \
+        { \
+            *((void **)&(__array)) = m_array_grow(__array); \
+        } \
+        (__array)[m_array_size(__array)] = (__data); \
+        m_array_head(__array)->size++; \
+    } while (0)
 
+// "Pops" data off of the array (decrements the size)
 #define m_array_pop(__array) \
-	do \
-	{ \
-		if (__array && !m_array_empty(__array)) \
-		m_array_head(__array)->size -= 1; \
-	} while(0)
+    do \
+    { \
+        if (__array && !m_array_empty(__array)) \
+        m_array_head(__array)->size -= 1; \
+    } while(0)
 
-#define m_array(__type)	__type *
+// Clears all the elements in the array, (simply sets size to 0)
+#define m_array_clear(__array) \
+    do \
+    { \
+        if (__array)\
+            m_array_head(__array)->size = 0; \
+    } while (0)
 
 /////////////////////////////////
 // ====== IMPL =========== //
@@ -65,51 +93,51 @@ void *m_array_resize(void *arr, usize sz, usize amt);
 
 void**
 m_array_init(void **arr,
-			 usize val_size)
+             usize val_size)
 {
-	if (*arr == NULL)
-	{
-		m_array_header_t *data;
-		data = (m_array_header_t *)malloc(val_size + sizeof(m_array_header_t));
+    if (*arr == NULL)
+    {
+        m_array_header_t *data;
+        data = (m_array_header_t *)malloc(val_size + sizeof(m_array_header_t));
 
-		if (data)
-		{
-			data->size = 0;
-			data->capacity = 1;
-			*arr = ((u32 *)data + 2);
+        if (data)
+        {
+            data->size = 0;
+            data->capacity = 1;
+            *arr = ((u32 *)data + 2);
 
-			return arr;
-		}
-	}
+            return arr;
+        }
+    }
 
-	return NULL;
+    return NULL;
 }
 
 void*
 m_array_resize(void *arr,
-			   usize sz,
-			   usize amt)
+               usize sz,
+               usize amt)
 {
-	usize capacity;
-	m_array_header_t *data;
+    usize capacity;
+    m_array_header_t *data;
 
-	if (arr)
-		capacity = amt;
-	else
-		capacity = 0;
+    if (arr)
+        capacity = amt;
+    else
+        capacity = 0;
 
-	// Create new array with just the header
-	data = (m_array_header_t *)realloc(arr ? m_array_head(arr) : 0, capacity * sz + sizeof(m_array_header_t));
+    // Create new array with just the header
+    data = (m_array_header_t *)realloc(arr ? m_array_head(arr) : 0, capacity * sz + sizeof(m_array_header_t));
 
-	if (data)
-	{
-		if (!arr)
-			data->size = 0;
-		data->capacity = (u32)capacity;
-		data = (m_array_header_t *)data + 1;
-	}
+    if (data)
+    {
+        if (!arr)
+            data->size = 0;
+        data->capacity = (u32)capacity;
+        data = (m_array_header_t *)data + 1;
+    }
 
-	return data;
+    return data;
 }
 
  #endif // MHO_ARRAY_IMPL
